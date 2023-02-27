41a82cb4bdf440bbc49a9174a26dad0677cba733
Alamgir Ahamed <alamgir72435@gmail.com>
2023-02-27 08:06:35 +0600

    updates

--- micro-s-swarm/infra/build.sh
+++ micro-s-swarm/infra/build.sh
@@ -9,10 +9,10 @@ cd ../;
 cd collection-process;
 docker build -t collection-process-server .
 cd ../;
-# docker stack deploy -c docker-compose.yaml somity;
+docker stack deploy -c docker-compose.yaml somity;
 # Create Network
-docker network create -d overlay --attachable somity;
+# docker network create -d overlay --attachable somity;
 # Create Nats Deployment
-docker service create -d -p 4222:4222 --name nats-depl --network somity nats-streaming:0.17.0;
+# docker service create -d -p 4222:4222 --name nats-depl --network somity nats-streaming:0.17.0;
 # // Create main service
-docker service create -d -p 6000:6000 --name somity-server --network somity somity-server;
+# docker service create -d -p 6000:6000 --name somity-server --network somity somity-server;
